0887d4c8f9c0e8c38528ab3e5d8e14f6011b2bc1
Zsolt <57398867+Zsolt821201@users.noreply.github.com>
2023-04-15 19:11:16 +0100

    starter code

---
+++ src/main/java/io/github/zsolt821201/springsecurityassigment/SpringSecurityAssigmentApplication.java
@@ -0,0 +1,13 @@
+package io.github.zsolt821201.springsecurityassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAssigmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAssigmentApplication.class, args);
+	}
+
+}
